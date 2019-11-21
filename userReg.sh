33aa243ea8d0b5e659998e5cc3a817bc40c9c568
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-21 09:55:39 -0500

    Add: Valid Last name start with capital letter

--- userReg.sh
+++ userReg.sh
@@ -2,11 +2,15 @@
 shopt -s extglob
 echo "Welcome in user registration"
 
-echo "Enter the word ending with thing"
-read word
+#echo "Enter the last name"
+#read first
+echo "Enter last name"
+read last
 
-pat="^[A-Z][a-z]{3,}"
-if [[ $word =~ $pat ]];
+pat1="^[A-Z][a-z]{3,}"
+pat2="^[A-Z][a-z]{3,}"
+
+if [[ $last =~ $pat2 ]];
 then
 	echo "valid"
 else
