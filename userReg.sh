23ba5eeb3b3e10878b90fe639ae1acb9186ae164
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-21 09:50:19 -0500

    Add: Enter valid 1st name with capital letter

--- userReg.sh
+++ userReg.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+shopt -s extglob
 echo "Welcome in user registration"
+
+echo "Enter the word ending with thing"
+read word
+
+pat="^[A-Z][a-z]{3,}"
+if [[ $word =~ $pat ]];
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
