a667dc7f52ff5ec9c13e2dab149e06f9030e221e
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-21 18:02:06 -0500

    Add : Atleast on Captial letter

--- userReg.sh
+++ userReg.sh
@@ -1,25 +1,26 @@
-#!/bin/bash
+#!/bin/bash -x
 shopt -s extglob
 echo "Welcome in user registration"
 
-#echo "Enter the last name"
-#read first
-#echo "Enter last name"
-#read last
-#echo "Enter valid Email"
-#read email
-#echo "Enter mob Number"
-#read mobNo
-echo "Enter password"
-read pass
-pat1="^[A-Z][a-z]{3,}"
-pat2="^[A-Z][a-z]{3,}"
-pat3="^[a-zA-Z][a-zA-Z0-9._-+]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
-pat4="^[0-9]{2}\s[0-9]{10}"
-pat5="^[a-zA-Z0-9]{8,}"
-if [[ $pass =~ $pat5 ]];
-then
-	echo "valid"
-else
-	echo "invalid"
-fi
+while [ true ]
+do
+	#read -p "Enter the First name :" first
+	#read -p "Enter last name :" last
+	#read -p "Enter valid Email :" email
+	#read -p "Enter mob Number :" mobNo
+	read -p "Enter password :" pass
+
+	pat1="^[A-Z]{1}[a-z]{3,}$"
+	pat2="^[A-Z]{1}[a-z]{3,}$"
+	pat3="^[a-zA-Z][a-zA-Z0-9._-+]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
+	pat4="^[0-9]{2}\s[0-9]{10}$"
+	pat5="^[a-zA-Z0-9]{8,}$"
+	pat6="^([a-zA-Z0-9]*[A-Z]+[a-zA-Z0-9]*)$"
+
+	if [[ $pass =~ $pat6 ]]
+	then
+		echo "valid"
+	else
+		echo "invalid"
+	fi
+done
