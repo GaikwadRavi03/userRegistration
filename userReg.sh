444e593629348d48a40b57c6dda2ef927718a699
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-21 10:05:04 -0500

    Add: vlaid mob no with country code

--- userReg.sh
+++ userReg.sh
@@ -4,13 +4,18 @@ echo "Welcome in user registration"
 
 #echo "Enter the last name"
 #read first
-echo "Enter last name"
-read last
-
+#echo "Enter last name"
+#read last
+#echo "Enter valid Email"
+#read email
+echo "Enter mob Number"
+read mobNo
 pat1="^[A-Z][a-z]{3,}"
 pat2="^[A-Z][a-z]{3,}"
+pat3="^[a-zA-Z][a-zA-Z0-9._-+]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
+pat4="^[0-9]{2}\s[0-9]{10}"
 
-if [[ $last =~ $pat2 ]];
+if [[ $mobNo =~ $pat4 ]];
 then
 	echo "valid"
 else
