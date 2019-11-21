40f244f4b518d0c7634fab0d05742afa0d6a4bc7
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-21 10:11:19 -0500

    Add: Password min 8 char

--- userReg.sh
+++ userReg.sh
@@ -8,14 +8,16 @@ echo "Welcome in user registration"
 #read last
 #echo "Enter valid Email"
 #read email
-echo "Enter mob Number"
-read mobNo
+#echo "Enter mob Number"
+#read mobNo
+echo "Enter password"
+read pass
 pat1="^[A-Z][a-z]{3,}"
 pat2="^[A-Z][a-z]{3,}"
 pat3="^[a-zA-Z][a-zA-Z0-9._-+]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]{2,4}[.]{1}*[a-zA-Z]{2,3}*$"
 pat4="^[0-9]{2}\s[0-9]{10}"
-
-if [[ $mobNo =~ $pat4 ]];
+pat5="^[a-zA-Z0-9]{8,}"
+if [[ $pass =~ $pat5 ]];
 then
 	echo "valid"
 else
